358ccae1da44177c021ab5214e398d31018a6303
franck403 <79372051+franck403@users.noreply.github.com>
2023-03-11 16:01:02 -0500

    Update sw.hitsab.js

--- sw.hitsab.js
+++ sw.hitsab.js
@@ -1,9 +1,7 @@
-var proxy_url = 'https://cors-anywhere.herokuapp.com/';
-
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
-      return res || fetch(proxy_url + fetchEvent.request)
+      return res || fetch('https://cors-anywhere.herokuapp.com/' + fetchEvent.request)
     })
   )
 })
